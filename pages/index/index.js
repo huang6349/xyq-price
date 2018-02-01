fbc90f98bf26e854c35c7154513d62a25803ba95
huang6349 <252683858@qq.com>
2018-02-01 16:22:47 +0800

    完成基本布局

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,90 @@
 //index.js
 //获取应用实例
-const app = getApp()
+const app = getApp();
+const { extend, Tab } = require('../../zanui/2.4.4/dist/index');
 
-Page({
+Page(extend({}, Tab, {
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function () {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+    mainHeight: '100%',
+    loadmore: {},
+    tab1: {
+      list: [
+        {
+          id: '1',
+          title: '最新商品1'
+        },
+        {
+          id: '2',
+          title: '最新商品2'
+        },
+        {
+          id: '3',
+          title: '最新商品3'
+        },
+        {
+          id: '4',
+          title: '最新商品4'
+        },
+        {
+          id: '5',
+          title: '最新商品5'
+        },
+        {
+          id: '6',
+          title: '最新商品6'
         }
-      })
-    }
+      ],
+      selectedId: '1',
+      scroll: true,
+      height: 45,
+    },
+    prices: [
+      {
+        "name": "60武器",
+        "price": 150000,
+      },
+      {
+        "name": "60装备",
+        "price": 115000,
+      },
+    ],
   },
-  getUserInfo: function (e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  onLoad: function () { // 页面渲染后 执行
+    this.nomoreShow();
+  },
+  onShow: function () {
+
+  },
+  loadingShow: function () {
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
-})
+      loadmore: {
+        loading: true,
+      },
+    });
+  },
+  nodataShow: function () {
+    this.setData({
+      loadmore: {
+        nodata: true,
+      },
+    });
+  },
+  nomoreShow: function () {
+    this.setData({
+      loadmore: {
+        nomore: true,
+      },
+    });
+  },
+  loadmoreHide: function () {
+    this.setData({
+      loadmore: {
+      },
+    });
+  },
+  handleZanTabChange: function ({ componentId, selectedId }) {
+    this.setData({
+      [`${componentId}.selectedId`]: selectedId,
+    });
+  },
+}));
