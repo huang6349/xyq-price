f6308c58dea38e77d600e05f7081570f98967672
huang6349 <252683858@qq.com>
2018-02-01 20:24:54 +0800

    完成初版

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,52 +6,86 @@ const { GET } = require('../../utils/http.js');
 
 Page(extend({}, Tab, Toast, {
   data: {
-    mainHeight: '100%',
-    loadmore: {},
+    loadmore: {
+      loading: true,
+    },
     tab1: {
-      list: [],
-      selectedId: '1',
+      list: [
+        {
+          id: 0,
+          title: '无',
+        }
+      ],
+      selectedId: 0,
       scroll: true,
       height: 45,
     },
-    prices: [
-      {
-        "name": "60武器",
-        "price": 150000,
-      },
-      {
-        "name": "60装备",
-        "price": 115000,
-      },
-    ],
+    prices: [],
   },
   onLoad: function () { // 页面渲染后 执行
     GET('/index.json', (res) => {
-      if (res) {
-        console.log(res);
-        let index = res[0];
-        GET(index.uri, (res2) => {
-          if (res2) {
-            this.showZanToast(`获得${res2['date']}的数据`);
-            console.log(res2['list'][0]['id']);
-            this.setData({
-              tab1: {
-                list: res2['list'],
-                selectedId: res2['list'][0]['id'],
-              },
-            });
-          } else {
-            this.showZanToast(`请求${index.uri}失败`);
-          }
+      if (!res) {
+        this.nodataShow();
+        this.showZanToast('请求服务器列表失败');
+        return;
+      }
+      let index = res[0];
+      if (!index) {
+        this.nodataShow();
+        this.showZanToast('请求服务器列表失败');
+        return;
+      }
+      GET(index.uri, (res2) => {
+        if (!res2) {
+          this.nodataShow();
+          this.showZanToast(`请求商品分类失败`);
+          return;
+        }
+        if (!res2['list'] || res2['list'].length === 0) {
+          this.nodataShow();
+          this.showZanToast(`请求商品分类失败`);
+          return;
+        }
+        this.showZanToast(`获得[${index['name']}][${res2['date']}]的数据`);
+        let selectedId = res2['list'][0]['id'];
+        this.setData({
+          tab1: {
+            list: res2['list'],
+            selectedId: res2['list'][0]['id'],
+          },
         });
-      } else {
-        this.showZanToast('请求index.json失败');
+        this.loaPrice(selectedId);
+      });
+    });
+  },
+  loaPrice: function (selectedId) {
+    if (!this.data || !this.data.tab1 || !this.data.tab1.list) {
+      this.nodataShow();
+      this.showZanToast(`请求商品分类无效`);
+      return;
+    }
+    this.loadingShow();
+    let uri;
+    this.data.tab1.list.forEach((item, index) => {
+      if (selectedId === item['id']) {
+        uri = item['uri'];
+        return;
       }
     });
-    this.nomoreShow();
-  },
-  onShow: function () {
-
+    GET(uri, (res) => {
+      this.loadmoreHide();
+      if (!res) {
+        this.nodataShow();
+        this.showZanToast(`请求商品价格失败`);
+        return;
+      }
+      if (res.length === 0) {
+        this.nodataShow();
+      }
+      this.setData({
+        prices: res,
+      });
+    });
   },
   loadingShow: function () {
     this.setData({
@@ -84,5 +118,6 @@ Page(extend({}, Tab, Toast, {
     this.setData({
       [`${componentId}.selectedId`]: selectedId,
     });
+    this.loaPrice(selectedId);
   },
 }));
--- utils/http.js
+++ utils/http.js
@@ -1,4 +1,4 @@
-const root = "https://raw.githubusercontent.com/huang6349/xyq-price-data/master";
+const root = "https://gitee.com/252683858/xyq-price-data/raw/master";
 
 const GET = (url, cb) => {
   wx.request({
